1a2500a356c417ae3d1639f8a251091ac5af8912
Sauzan Faza <sauzanfaza@gmail.com>
2026-02-20 10:36:16 +0700

    setup tailwind, create navbar

---
+++ Imas-que-fe/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { LuDonut } from "react-icons/lu";
+
+export default function Navbar() {
+    return(
+        <div className="w-full flex gap-5 p-10">
+            <LuDonut className="text-5xl"/>
+            <h1 className="text-4xl font-medium">Imas Que</h1>
+        </div>
+    )
+}
